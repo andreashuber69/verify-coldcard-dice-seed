0acf453749e2651ba82cfb426b183a846546e2c4
Andreas Huber <andreashuber69@users.noreply.github.com>
2021-07-05 21:41:23 +0200

    feat: calculate words

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,4 @@
+import { wordlists } from "bip39";
 import sha256 from "crypto-js/sha256";
 
 class App {
@@ -8,12 +9,17 @@ class App {
             const hexEntropy = entropy.toString();
             console.log(`Entropy: ${hexEntropy}`);
 
-            // The following operations implement https://en.bitcoin.it/wiki/BIP_0039:
-            // 1. Calculate checksum: take the first byte (2 hex digits) of the sha256
+            // The following steps implement https://en.bitcoin.it/wiki/BIP_0039:
+
+            // 1. Calculate checksum: take the first byte (2 hexadecimal digits) of the sha256
             const hexChecksum = sha256(entropy).toString().slice(0, 2);
+
             // 2. Append checksum to the end of entropy
             const hexEntropyWithChecksum = hexEntropy + hexChecksum;
-            console.log(`Entropy with checksum: ${hexEntropyWithChecksum}`);
+
+            // 3. Slice into 24 words, representing 11 bits each
+            const words = this.toWords(hexEntropyWithChecksum);
+            console.log(words);
 
             return 0;
         } catch (ex: unknown) {
@@ -28,7 +34,7 @@ class App {
     private static async requestInput(prompt: string) {
         process.stdout.write(prompt);
 
-        return await App.getConsoleInput();
+        return await this.getConsoleInput();
     }
 
     private static async getConsoleInput() {
@@ -40,9 +46,21 @@ class App {
             });
         });
     }
+
+    private static toWords(hexEntropy: string): readonly string[] {
+        const allWords = wordlists.english;
+        const words = new Array<string>(hexEntropy.length * 4 / Math.log2(allWords.length));
+        const divisor = BigInt(allWords.length);
+        let entropy = BigInt(`0x${hexEntropy}`);
+
+        for (let index = words.length - 1; index >= 0; --index) {
+            words[index] = allWords[Number(entropy % divisor)];
+            entropy /= divisor;
+        }
+
+        return words;
+    }
 }
 
 // The catch should never be reached (because we handle all errors in main). If it does, we let the whole thing fail.
 App.main().then((exitCode) => (process.exitCode = exitCode)).catch(() => (process.exitCode = 1));
-
-export {};
