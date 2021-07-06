806bdfcd62de1c1b5fc64ac2180af3b3bbc62752
Andreas Huber <andreashuber69@users.noreply.github.com>
2021-07-06 21:41:55 +0200

    refactor(calculateBip39Mnemonic): extract method

--- src/App.ts
+++ src/App.ts
@@ -1,24 +1,11 @@
-import { wordlists } from "bip39";
 import sha256 from "crypto-js/sha256";
+import { calculateBip39Mnemonic } from "./calculateBip39Mnemonic";
 
 class App {
     public static async main() {
         try {
             const input = await this.requestInput("Dice rolls: ");
-            const entropy = sha256(input);
-            const hexEntropy = entropy.toString();
-            console.log(`Entropy: ${hexEntropy}`);
-
-            // The following steps implement https://en.bitcoin.it/wiki/BIP_0039:
-
-            // 1. Calculate checksum: take the first byte (2 hexadecimal digits) of the sha256
-            const hexChecksum = sha256(entropy).toString().slice(0, 2);
-
-            // 2. Append checksum to the end of entropy
-            const hexEntropyWithChecksum = hexEntropy + hexChecksum;
-
-            // 3. Slice into 24 words, representing 11 bits each
-            const words = this.toWords(hexEntropyWithChecksum);
+            const words = calculateBip39Mnemonic(sha256(input).toString());
             console.log(words);
 
             return 0;
@@ -45,20 +32,6 @@ class App {
                 stdin.pause();
             });
         });
-    }
-
-    private static toWords(hexEntropy: string): readonly string[] {
-        const allWords = wordlists.english;
-        const words = new Array<string>(hexEntropy.length * 4 / Math.log2(allWords.length));
-        const divisor = BigInt(allWords.length);
-        let entropy = BigInt(`0x${hexEntropy}`);
-
-        for (let index = words.length - 1; index >= 0; --index) {
-            words[index] = allWords[Number(entropy % divisor)];
-            entropy /= divisor;
-        }
-
-        return words;
     }
 }
 
---
+++ src/calculateBip39Mnemonic.ts
@@ -0,0 +1,52 @@
+import { wordlists } from "bip39";
+import { enc } from "crypto-js";
+import sha256 from "crypto-js/sha256";
+
+const toBigInt = (hexNumber: string) => BigInt(`0x${hexNumber}`);
+
+const calculateCheckSum = (hexEntropy: string, cs: number) => {
+    const entropySha256 = sha256(enc.Hex.parse(hexEntropy));
+
+    return toBigInt(`${entropySha256}`) >> BigInt((entropySha256.sigBytes * 8) - cs);
+};
+
+const getWords = (checkedEntropy: bigint, bits: number) => {
+    const allWords = wordlists.english;
+    const bitsPerWord = Math.log2(allWords.length);
+
+    if (bits % bitsPerWord !== 0) {
+        throw new RangeError("checkedEntropy has an unexpected number of bits");
+    }
+
+    const words = new Array<string>(bits / bitsPerWord);
+    const divisor = BigInt(allWords.length);
+
+    for (let index = words.length - 1; index >= 0; --index) {
+        words[index] = allWords[Number(checkedEntropy % divisor)];
+        // eslint-disable-next-line no-param-reassign
+        checkedEntropy /= divisor;
+    }
+
+    return words;
+};
+
+/**
+ * @description Calculates the mnemonic from the given entropy according to BIP-0039
+ * (https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki).
+ * @param hexEntropy The entropy to use for the mnemonic as a hexadecimal string. Due to constraints imposed by
+ * BIP-0039, the string length must be a multiple of 8. In violation of BIP-0039 however, for testing purposes it is
+ * allowed to pass strings with lengths smaller than 32.
+ */
+export const calculateBip39Mnemonic = (hexEntropy: string) => {
+    if (hexEntropy.length % 8 !== 0) {
+        throw new RangeError("hexEntropy length must be a multiple of 8");
+    }
+
+    // The following steps implement https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki:
+    const ent = hexEntropy.length * 4;
+    const cs = ent / 32;
+    // Shift left by cs bits to make room for checksum
+    const entropy = toBigInt(hexEntropy) << BigInt(cs);
+
+    return getWords(entropy + calculateCheckSum(hexEntropy, cs), ent + cs);
+};
