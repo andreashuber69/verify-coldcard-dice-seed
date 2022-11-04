b74748e363e032d162daadd5ad2beae183dbba40
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-04 11:43:39 +0100

    refactor: shorten implementation

--- src/toBech32Address.ts
+++ src/toBech32Address.ts
@@ -3,10 +3,5 @@ import { crypto } from "@bitgo/utxo-lib";
 import { bech32 } from "bech32";
 import type { BIP32Interface } from "bip32";
 
-export const toBech32Address = (node: BIP32Interface) => {
-    const hash160 = crypto.hash160(node.publicKey);
-    const bech32Words = bech32.toWords(hash160);
-    const words = new Uint8Array([0, ...bech32Words]);
-
-    return bech32.encode("bc", words);
-};
+export const toBech32Address = (node: BIP32Interface) =>
+    bech32.encode("bc", new Uint8Array([0, ...bech32.toWords(crypto.hash160(node.publicKey))]));
