2d7fa72cfde6190b11c2d0cc2415e6614262d92f
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-04 11:40:14 +0100

    refactor: reimplement toBech32Address

--- src/getAddresses.ts
+++ src/getAddresses.ts
@@ -1,8 +1,8 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed#--
-import type { HDNode } from "@bitgo/utxo-lib";
+import type { BIP32Interface } from "bip32";
 import { toBech32Address } from "./toBech32Address";
 
-export const getAddresses = (rootNode: HDNode, accountRootPath: string, startIndex: number, length: number) => {
+export const getAddresses = (rootNode: BIP32Interface, accountRootPath: string, startIndex: number, length: number) => {
     const accountRoot = rootNode.derivePath(accountRootPath);
     const result = new Array<[string, string]>();
 
--- src/toBech32Address.ts
+++ src/toBech32Address.ts
@@ -1,6 +1,12 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed#--
-import type { HDNode } from "@bitgo/utxo-lib";
-import { address, crypto, script } from "@bitgo/utxo-lib";
+import { crypto } from "@bitgo/utxo-lib";
+import { bech32 } from "bech32";
+import type { BIP32Interface } from "bip32";
 
-export const toBech32Address = (node: HDNode) =>
-    address.fromOutputScript(script.witnessPubKeyHash.output.encode(crypto.hash160(node.getPublicKeyBuffer())));
+export const toBech32Address = (node: BIP32Interface) => {
+    const hash160 = crypto.hash160(node.publicKey);
+    const bech32Words = bech32.toWords(hash160);
+    const words = new Uint8Array([0, ...bech32Words]);
+
+    return bech32.encode("bc", words);
+};
