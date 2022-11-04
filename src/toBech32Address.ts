bac5b43cef3f6ab0cba4b95bf0bcb9188e9578dc
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-04 12:13:36 +0100

    refactor: use parameter destructuring

--- src/toBech32Address.ts
+++ src/toBech32Address.ts
@@ -3,5 +3,5 @@ import { crypto } from "@bitgo/utxo-lib";
 import { bech32 } from "bech32";
 import type { BIP32Interface } from "bip32";
 
-export const toBech32Address = (node: BIP32Interface) =>
-    bech32.encode("bc", new Uint8Array([0, ...bech32.toWords(crypto.hash160(node.publicKey))]));
+export const toBech32Address = ({ publicKey }: BIP32Interface) =>
+    bech32.encode("bc", new Uint8Array([0, ...bech32.toWords(crypto.hash160(publicKey))]));
