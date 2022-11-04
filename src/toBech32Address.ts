63f835028f9dca7128fee402fe322fb3c206efd3
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-04 13:05:04 +0100

    refactor: use bitcoinjs-lib

--- src/toBech32Address.ts
+++ src/toBech32Address.ts
@@ -1,7 +1,7 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed#--
-import { crypto } from "@bitgo/utxo-lib";
 import { bech32 } from "bech32";
 import type { BIP32Interface } from "bip32";
+import { crypto } from "bitcoinjs-lib";
 
 export const toBech32Address = ({ publicKey }: BIP32Interface) =>
     bech32.encode("bc", new Uint8Array([0, ...bech32.toWords(crypto.hash160(publicKey))]));
