e52ddd45bc08c6d0175a63020c205052cf529fe8
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-07 13:26:14 +0100

    fix: line length

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ import { waitForUser } from "./waitForUser.js";
 const { stdin, stdout } = process;
 
 try {
-    // Simple typescript alternatives to calling require below lead to the outDir containing the file package.json and the
-    // directory src with all the code. This is due to how the ts compiler automatically determines the rootDir from
+    // Simple typescript alternatives to calling require below lead to the outDir containing the file package.json and
+    // the directory src with all the code. This is due to how the ts compiler automatically determines the rootDir from
     // imports. There are alternatives to calling require, but these seem overly complicated:
     // https://stackoverflow.com/questions/58172911/typescript-compiler-options-trying-to-get-flat-output-to-outdir
     const { version } = createRequire(import.meta.url)("../package.json") as { readonly version: string };
