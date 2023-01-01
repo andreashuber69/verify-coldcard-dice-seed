8fe1bbc34bddc82f858e426a966a17bada416b5d
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-01-01 17:40:31 +0100

    chore: update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed#--
 module.exports = {
-    extends: [
-        "@andreashuber69",
-    ],
+    env: {
+        node: true,
+    },
+    extends: ["@andreashuber69"],
 };
