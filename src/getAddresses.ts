fc5d739ca43309e8e84f90fe7a026228916a0b8a
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-01-07 13:09:17 +0100

    refactor: rename parameter

--- src/getAddresses.ts
+++ src/getAddresses.ts
@@ -4,9 +4,8 @@ import { toBech32Address } from "./toBech32Address.js";
 
 type Batch = [readonly [string, string], ...Array<readonly [string, string]>];
 
-export const getAddresses =
-(rootNode: BIP32Interface, accountRootPath: string, startIndex: number): Readonly<Batch> => {
-    const accountRoot = rootNode.derivePath(accountRootPath);
+export const getAddresses = (root: BIP32Interface, accountRootPath: string, startIndex: number): Readonly<Batch> => {
+    const accountRoot = root.derivePath(accountRootPath);
     const length = 10;
     const result: Batch = [["", ""], ...new Array<[string, string]>(length - 1)];
 
