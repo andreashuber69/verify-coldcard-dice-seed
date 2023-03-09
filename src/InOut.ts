12c2e3b9fec0322ed328df815c07ef79c2c27eb9
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-03-09 20:49:48 +0100

    refactor: simplify interfaces

--- src/InOut.ts
+++ src/InOut.ts
@@ -1,17 +1,13 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed/blob/develop/README.md#----verify-coldcard-dice-seed
-type InNames = "once" | "pause" | "resume";
-
-export type GenericIn<T extends keyof NodeJS.ReadStream> = Pick<NodeJS.ReadStream, T>;
-
-export type GenericOut<T extends keyof NodeJS.WriteStream> = Pick<NodeJS.WriteStream, T>;
+type GenericOut<T extends keyof NodeJS.WriteStream> = Pick<NodeJS.WriteStream, T>;
 
 export interface GenericInOut<T, U> {
     readonly stdin: T;
     readonly stdout: U;
 }
 
-export type In = GenericIn<InNames>;
+export type In = Pick<NodeJS.ReadStream, "once" | "pause" | "resume">;
 
-export type InOut = GenericInOut<GenericIn<InNames>, GenericOut<"write">>;
+export type InOut = GenericInOut<In, GenericOut<"write">>;
 
-export type InMovableOut = GenericInOut<GenericIn<InNames>, GenericOut<"moveCursor" | "write">>;
+export type InMovableOut = GenericInOut<In, GenericOut<"moveCursor" | "write">>;
