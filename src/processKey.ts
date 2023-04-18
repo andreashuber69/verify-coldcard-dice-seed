42ee8f8a7d19d1ab49a4bd965ab623486e4fe528
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-04-18 14:03:59 +0200

    feat: adapt to 5.1.2 firmware

--- src/processKey.ts
+++ src/processKey.ts
@@ -5,7 +5,9 @@ import { sha256 } from "./sha256.js";
 
 export const processKey = async ({ stdin, stdout }: InOut, input: string): Promise<[string, string]> => {
     stdout.write(`${input.length} rolls\r\n`);
-    stdout.write(`${sha256(Buffer.from(input))}\r\n\r\n`);
+    stdout.write(`${sha256(Buffer.from(input))}\r\n`);
+    const canFinish = input.length >= 99;
+    stdout.write(`Press 1-6 for each roll to mix in${canFinish ? ", ENTER to finish" : ""} or CTRL-C to abort.\r\n`);
     const key = await getKey(stdin);
-    return [`${input}${key >= "1" && key <= "6" ? key : ""}`, key];
+    return [`${input}${key >= "1" && key <= "6" ? key : ""}`, canFinish ? key : ""];
 };
