9e88468f0a044509f90264df2caefb9de3bce0a6
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-08-13 10:50:44 +0200

    refactor: remove mocha & chai dependecies

--- src/calculateBip39Mnemonic.spec.ts
+++ src/calculateBip39Mnemonic.spec.ts
@@ -1,7 +1,7 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed/blob/develop/README.md#----verify-coldcard-dice-seed
+import assert from "node:assert";
+import { describe, it } from "node:test";
 import { wordlists } from "bip39";
-import { expect } from "chai";
-import Mocha from "mocha";
 import fetch from "node-fetch";
 
 import { calculateBip39Mnemonic } from "./calculateBip39Mnemonic.js";
@@ -13,25 +13,22 @@ if (!wordlist) {
     throw new Error("Missing english wordlist.");
 }
 
-const addPassingTest = (s: Mocha.Suite, entropy: string, words: string) => s.addTest(
-    new Mocha.Test(entropy, () => expect(calculateBip39Mnemonic(entropy, wordlist).join(" ")).to.equal(words)),
+const addPassingTest = (entropy: string, words: string) =>
+    it(entropy, () => assert(calculateBip39Mnemonic(entropy, wordlist).join(" ") === words));
+
+
+const addFailingTest = (entropy: string, newWordlist: readonly string[], errorMessage: string) => it(
+    entropy,
+    () => {
+        try {
+            calculateBip39Mnemonic(entropy, newWordlist);
+            assert(false, "Expected error to be thrown!");
+        } catch (error: unknown) {
+            assert(error instanceof RangeError && error.message === errorMessage);
+        }
+    },
 );
 
-const addFailingTest = (
-    s: Mocha.Suite,
-    entropy: string,
-    newWordlist: readonly string[],
-    errorMessage: string,
-) => s.addTest(
-    new Mocha.Test(
-        entropy,
-        () => expect(() => calculateBip39Mnemonic(entropy, newWordlist)).to.throw(RangeError, errorMessage),
-    ),
-);
-
-
-const mocha = new Mocha();
-const suite = Mocha.Suite.create(mocha.suite, "calculateBip39Mnemonic");
 
 const response = await fetch("https://raw.githubusercontent.com/trezor/python-mnemonic/master/vectors.json");
 
@@ -41,31 +38,30 @@ if (!response.ok) {
 
 const vectors = JSON.parse(await response.text()) as Record<string, unknown>;
 
-if (!("english" in vectors) || !Array.isArray(vectors["english"])) {
-    throw new Error("Unexpected response");
-}
-
-for (const vector of vectors["english"]) {
-    if (!Array.isArray(vector) || (vector.length < 2) ||
-        (typeof vector[0] !== "string") || (typeof vector[1] !== "string")) {
+describe(calculateBip39Mnemonic.name, () => {
+    if (!("english" in vectors) || !Array.isArray(vectors["english"])) {
         throw new Error("Unexpected response");
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const [entropy, words] = vector;
-    addPassingTest(suite, entropy, words);
-}
+    for (const vector of vectors["english"]) {
+        if (!Array.isArray(vector) || (vector.length < 2) ||
+            (typeof vector[0] !== "string") || (typeof vector[1] !== "string")) {
+            throw new Error("Unexpected response");
+        }
 
-addPassingTest(suite, "", "");
-addPassingTest(suite, "00000000", "abandon abandon ability");
-addPassingTest(suite, "ffffffff", "zoo zoo zoo");
-addFailingTest(suite, "3", wordlist, "hexEntropy length must be a multiple of 8");
-addFailingTest(suite, "777777777", wordlist, "hexEntropy length must be a multiple of 8");
-addFailingTest(suite, "ffffffff", wordlist.slice(1), "wordlist.length is invalid: 2047");
-addFailingTest(suite, "ffffffff", wordlist.slice(1024), "wordlist.length is invalid: 1024");
-const invalidWordlist = wordlist.slice(-1);
-invalidWordlist.push("");
-addFailingTest(suite, "ffffffff", invalidWordlist, "wordlist is invalid");
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const [entropy, words] = vector;
+        addPassingTest(entropy, words);
+    }
 
-const suiteRun = mocha.run();
-process.on("exit", () => process.exit(suiteRun.stats?.failures ?? 0));
+    addPassingTest("", "");
+    addPassingTest("00000000", "abandon abandon ability");
+    addPassingTest("ffffffff", "zoo zoo zoo");
+    addFailingTest("3", wordlist, "hexEntropy length must be a multiple of 8");
+    addFailingTest("777777777", wordlist, "hexEntropy length must be a multiple of 8");
+    addFailingTest("ffffffff", wordlist.slice(1), "wordlist.length is invalid: 2047");
+    addFailingTest("ffffffff", wordlist.slice(1024), "wordlist.length is invalid: 1024");
+    const invalidWordlist = wordlist.slice(-1);
+    invalidWordlist.push("");
+    addFailingTest("ffffffff", invalidWordlist, "wordlist is invalid");
+});
