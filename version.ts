85053435651cba0b00c3e8b1e2b48427f31ae312
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-02 19:19:08 +0100

    chore: finish new versioning

--- version.ts
+++ version.ts
@@ -29,3 +29,10 @@ const newReadme = readme.
     replaceAll(`Verify COLDCARD Dice Seed v${previousVersion}`, `Verify COLDCARD Dice Seed v${version}`);
 
 await writeFile(readmeFilename, newReadme, encoding);
+
+// The command `npm version` modifies package.json & package-lock.json before calling this script. At this point, we
+// therefore have 4 modified files. Since `git flow release start` does not work with uncommitted changes, we first
+// need to stash the changes and pop them afterwards.
+await exec("git stash push");
+await exec(`git flow release start v${version}`);
+await exec("git stash pop");
