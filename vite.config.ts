f052a8be2717e43070e88e24fe4a53ba71a436df
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-02 15:44:43 +0100

    chore: split tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+import type { Plugin } from "vite";
 import { defineConfig } from "vite";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import wasm from "vite-plugin-wasm";
 
 const config = defineConfig({
     plugins: [
-        wasm(),
-        nodePolyfills()
+        (wasm as unknown as () => Plugin)(),
+        nodePolyfills(),
     ],
     build: {
         target: "esnext",
