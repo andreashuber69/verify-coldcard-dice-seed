c0847128f6cd65ed9eb519c8d2d499fa4994026e
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-04 20:36:57 +0100

    feat: first functional version

--- src/site.ts
+++ src/site.ts
@@ -1,8 +1,18 @@
 import { BIP32Factory } from "bip32";
-import { mnemonicToSeed } from "bip39";
+import { mnemonicToSeed, wordlists } from "bip39";
 // eslint-disable-next-line import/no-namespace
 import * as ecc from "tiny-secp256k1";
+import { calculateBip39Mnemonic } from "./calculateBip39Mnemonic.js";
 import { getAddresses } from "./getAddresses.js";
+import { sha256 } from "./sha256.js";
+
+const wordlist = wordlists["english"];
+
+if (!wordlist) {
+    // cSpell: ignore wordlist
+    throw new Error("Missing english wordlist.");
+}
+
 
 const getElement = <T extends HTMLElement>(ctor: abstract new () => T, selector: string) => {
     const result = document.querySelector(selector);
@@ -14,9 +24,62 @@ const getElement = <T extends HTMLElement>(ctor: abstract new () => T, selector:
     return result;
 };
 
-const words = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-const root = BIP32Factory(ecc).fromSeed(await mnemonicToSeed(words, ""));
-const batch = getAddresses(root, "m/84'/0'/0'/0", 0);
+const generate24WordsElement = getElement(HTMLInputElement, "#generate-24-words");
+const diceRollsElement = getElement(HTMLInputElement, "#dice-rolls");
+const passphraseElement = getElement(HTMLInputElement, "#passphrase");
+const rollsCountElement = getElement(HTMLDivElement, "#rolls-count");
+const hashElement = getElement(HTMLDivElement, "#hash");
+const addressesElement = getElement(HTMLDivElement, "#addresses");
 
-const listElement = getElement(HTMLElement, "#list");
-listElement.innerHTML = batch.map(([_, address]) => `<p class="monospace">${address}</li>`).join("");
+const getMnemonic = async (rolls: string) => {
+    rollsCountElement.textContent = `${rolls.length} rolls`;
+    hashElement.textContent = await sha256(new TextEncoder().encode(rolls));
+
+    if (diceRollsElement.ariaInvalid === "true") {
+        return undefined;
+    }
+
+    const wordCount = generate24WordsElement.checked ? 24 : 12;
+    return await calculateBip39Mnemonic(hashElement.textContent, wordCount, wordlist);
+};
+
+const showMnemonic = (mnemonic: string[] | undefined = undefined) => {
+    for (const [index, word] of (mnemonic ?? new Array<undefined>(24)).entries()) {
+        const wordNumber = `${index + 1}`.padStart(2, "0");
+        const element = getElement(HTMLElement, `#word${wordNumber}`);
+        element.textContent = word ? `${wordNumber}. ${word}` : "";
+    }
+};
+
+const bip32 = BIP32Factory(ecc);
+
+const onInput = async () => {
+    diceRollsElement.minLength = generate24WordsElement.checked ? 99 : 50;
+    const rolls = diceRollsElement.value;
+    const { tooShort, patternMismatch, valueMissing } = diceRollsElement.validity;
+    diceRollsElement.ariaInvalid = `${tooShort || patternMismatch || valueMissing}`;
+    const mnemonic = await getMnemonic(rolls);
+
+    if (mnemonic) {
+        showMnemonic(mnemonic);
+
+        const root = bip32.fromSeed(await mnemonicToSeed(mnemonic.join(" "), passphraseElement.value));
+        let batch: ReturnType<typeof getAddresses>;
+        let addressesHtml = "";
+
+        for (let startIndex = 0; startIndex < 50; startIndex += batch.length) {
+            batch = getAddresses(root, "m/84'/0'/0'/0", startIndex);
+            addressesHtml += batch.reduce((p, [path, addr]) => `${p}${path} => ${addr}<br>`, "");
+        }
+
+        addressesElement.innerHTML = addressesHtml;
+    } else {
+        showMnemonic();
+        addressesElement.innerHTML = "";
+    }
+};
+
+generate24WordsElement.addEventListener("input", () => void onInput());
+diceRollsElement.addEventListener("input", () => void onInput());
+passphraseElement.addEventListener("input", () => void onInput());
+await onInput();
