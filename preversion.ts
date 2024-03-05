b134816347e9f6e73ac27972698aa897864d8a5a
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-05 13:00:34 +0100

    refactor: deduplicate

--- preversion.ts
+++ preversion.ts
@@ -3,8 +3,10 @@ import { writeFile } from "node:fs/promises";
 import { promisify } from "node:util";
 import { version } from "./package.json";
 
+const encoding = { encoding: "utf8" } as const;
+
 const exec = async (command: string) => {
-    const { stdout, stderr } = await promisify(nodeExec)(command, { encoding: "utf8" });
+    const { stdout, stderr } = await promisify(nodeExec)(command, encoding);
     console.log(stdout);
     console.error(stderr);
 };
@@ -12,4 +14,4 @@ const exec = async (command: string) => {
 await exec("git checkout develop");
 await exec("git push");
 await exec("git pull");
-await writeFile("previousVersion.txt", version, { encoding: "utf8" });
+await writeFile("previousVersion.txt", version, encoding);
