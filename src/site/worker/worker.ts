b9e66e00e935e85549c6026e03db7748b0ed966d
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-04-21 19:31:19 +0200

    chore: upgrade to kiss-worker 1.0.0

--- src/site/worker/worker.ts
+++ src/site/worker/worker.ts
@@ -1,5 +1,5 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed/blob/develop/README.md#----verify-coldcard-dice-seed
-import { listen } from "kiss-worker";
+import { serve } from "kiss-worker";
 import { getAddressesForMnemonicAndPassphraseImpl } from "./getAddressesForMnemonicAndPassphraseImpl.js";
 
-listen(getAddressesForMnemonicAndPassphraseImpl);
+serve(getAddressesForMnemonicAndPassphraseImpl);
