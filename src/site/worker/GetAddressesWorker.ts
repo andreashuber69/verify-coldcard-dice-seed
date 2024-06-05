f8f2430e0aa5cae6962f78851b4cdfc59052c18d
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-06-05 16:36:24 +0200

    refactor: remove WebWorker lib

--- src/site/worker/GetAddressesWorker.ts
+++ src/site/worker/GetAddressesWorker.ts
@@ -1,5 +1,5 @@
 // https://github.com/andreashuber69/verify-coldcard-dice-seed/blob/develop/README.md#----verify-coldcard-dice-seed
-import { FunctionInfo, implementFunctionWorkerExternal } from "kiss-worker";
+import { FunctionInfo, implementFunctionWorkerExternal, Worker } from "kiss-worker";
 import type { getAddresses } from "./getAddresses.js";
 
 export const createGetAddressesWorker = implementFunctionWorkerExternal(
