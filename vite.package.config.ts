045eb07b8515c1d7ed2229679120f410f12dbf16
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-13 15:36:18 +0100

    build: move to vite build

---
+++ vite.package.config.ts
@@ -0,0 +1,23 @@
+// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+
+import { defineConfig } from "vite";
+
+// eslint-disable-next-line import/no-default-export, import/no-anonymous-default-export
+export default defineConfig({
+    build: {
+        lib: {
+            entry: ["src/package/main.ts"],
+            fileName: "main",
+            formats: ["es"],
+        },
+        outDir: "dist/package",
+        rollupOptions: {
+            input: {
+                main: "src/package/main.ts",
+            },
+        },
+        ssr: true,
+        target: "es2022",
+    },
+    publicDir: false,
+});
