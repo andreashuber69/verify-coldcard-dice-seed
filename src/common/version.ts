7f699145b2328bbeb45b4224308d1b72d0c7af0b
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-14 17:06:41 +0100

    chore: disable warning

--- src/common/version.ts
+++ src/common/version.ts
@@ -3,4 +3,5 @@
 // vite detects and converts dynamic imports such that the contents is bundled. This is desirable for the browser build
 // but a bit unfortunate for the node build (since package.json is available in the package anyway). Given the
 // comparatively small size increase, this is not worth further investigation.
+// eslint-disable-next-line import/dynamic-import-chunkname
 export const { version } = (await import("../../package.json", { assert: { type: "json" } })).default;
